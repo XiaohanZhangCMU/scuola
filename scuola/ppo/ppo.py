6aa21050a3432b04f21b476b8d692c67064d4371
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-04-11 23:15:52 -0700

    update

--- scuola/ppo/ppo.py
+++ scuola/ppo/ppo.py
@@ -602,12 +602,12 @@ def main(cfg: DictConfig):
             query_token_ids=episodes["all_query_token_ids"],
             response_token_ids=episodes["all_response_token_ids"],
             advantages=episodes["all_advantages"],
-            device=device.device_id,
+            device=device._device,
         )
 
         # Calculate losses and update model
         policy_model.train()
-        reference_model.to(device.device_id)
+        reference_model.to(device._device)
         reference_model.eval()
 
         total_response_len = (model_inputs["labels"] != -100).sum().item()
