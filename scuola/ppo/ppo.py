29667e144b4cc4213906d53ea9b0c1ca55caf10c
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-04-14 16:37:20 -0700

    update

--- scuola/ppo/ppo.py
+++ scuola/ppo/ppo.py
@@ -461,6 +461,9 @@ def main(cfg: DictConfig):
     )
     reference_model.eval()
 
+    policy_model = device.module_to_device(policy_model)
+    reference_model = device.module_to_device(reference_model)
+
     # Get tokenizer components
     EOS_TOKEN_ID = tokenizer.eos_token_id
     EOS_TOKEN = tokenizer.convert_ids_to_tokens(EOS_TOKEN_ID)
