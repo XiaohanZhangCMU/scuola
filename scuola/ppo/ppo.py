fb04d03e155747af05898714bd192a131c77ea3a
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-04-15 15:58:04 -0700

    update

--- scuola/ppo/ppo.py
+++ scuola/ppo/ppo.py
@@ -501,7 +501,7 @@ def main(cfg: DictConfig):
     _, reference_model = prepare_fsdp_module(
                     policy_model,
                     optimizer,
-                    FSDPConfig(**fsdp_config)
+                    FSDPConfig(**fsdp_config),
                     precision,
                     device,
                     auto_microbatching=False,
